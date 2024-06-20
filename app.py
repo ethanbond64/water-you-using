f2f3c351859a1d85b04c79e728324fa601e78af3
ethanbond64 <ethanbond39@gmail.com>
2024-06-19 22:13:01 -0600

    progress

--- app.py
+++ app.py
@@ -1,28 +1,49 @@
 import base64
 import re
+import threading
+import time
+import uuid
+
 from flask import Flask, render_template, request, url_for
+
+data = {}
 
 app = Flask(__name__)
 
+def load(uuid_str):
+    time.sleep(5)
+    data[uuid_str] = True
+
 @app.route('/')
 def camera():
-    return render_template("camera.html", link=url_for('submitted'))
+    return render_template("camera.html", submit_url=url_for('submit'))
 
-@app.route('/results')
-def submitted():
-    return "Photo submitted"
-
-@app.route('/submitted', methods=['POST'])
-def submitted():
+@app.route('/submit', methods=['POST'])
+def submit():
     image_data = request.form['image']
     image_data = re.sub('^data:image/.+;base64,', '', image_data)
     image_data = base64.b64decode(image_data)
 
-    # Save the image
-    with open('captured_image.png', 'wb') as f:
+    uuid_str = str(uuid.uuid4())
+    with open('imgs/' + uuid_str + '.png', 'wb') as f:
         f.write(image_data)
 
-    return "Photo submitted"
+    data[uuid_str] = False
+
+    threading.Thread(target=load, args=(uuid_str,)).start()
+
+    return render_template("loading.html", 
+                           status_url=url_for('status', uuid_str=uuid_str), 
+                           view_url=url_for('view', uuid_str=uuid_str))
+
+@app.route('/status/<uuid_str>')
+def status(uuid_str):
+    return {'status': data.get(uuid_str, False)}
+
+@app.route('/view/<uuid_str>')
+def view(uuid_str):
+    return render_template("view.html", 
+                           image_url='imgs/' + uuid_str + '.png')
 
 if __name__ == '__main__':
     app.run(debug=True)
